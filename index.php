7ce5d4534a59ca91b3799a7bd828e660ca0de102
flukfik777 <67219010011@loeitech.ac.th>
2026-01-28 10:15:08 +0700

    fdswer

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+require 'db.php';
+
+echo "worakit bureesang" ;
+
+try {
+    if ($pdo) {
+        echo "<h1>Connected to Database successfully!</h1>";
+        echo "<p>Database: stock_system</p>";
+        echo "<p>User: user</p>";
+    }
+} catch (Exception $e) {
+    echo "<h1>Connection Failed</h1>";
+    echo "<p>" . $e->getMessage() . "</p>";
+}
+?>
